0db833836d0b292c0df6d554c002a8b632539abc
ricky-ma <mr.rickyma@gmail.com>
2021-03-18 18:18:46 -0700

    Updated preprocessing pipeline for COCO dataset

---
+++ preprocessing.py
@@ -0,0 +1,145 @@
+from pycocotools.coco import COCO
+import numpy as np
+import random
+import skimage.io as io
+import cv2
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+def get_class_name(classID, cats):
+    for i in range(len(cats)):
+        if cats[i]['id'] == classID:
+            return cats[i]['name']
+    return None
+
+
+def filter_dataset(folder, classes=None, mode='train'):
+    # initialize COCO api for instance annotations
+    annFile = '{}/annotations/instances_{}.json'.format(folder, mode)
+    coco = COCO(annFile)
+    images = []
+    if classes is not None:
+        # iterate for each individual class in the list
+        for className in classes:
+            # get all images containing given categories
+            catIds = coco.getCatIds(catNms=className)
+            imgIds = coco.getImgIds(catIds=catIds)
+            images += coco.loadImgs(imgIds)
+    else:
+        imgIds = coco.getImgIds()
+        images = coco.loadImgs(imgIds)
+
+    # Now, filter out the repeated images
+    unique_images = []
+    for i in range(len(images)):
+        if images[i] not in unique_images:
+            unique_images.append(images[i])
+
+    random.shuffle(unique_images)
+    dataset_size = len(unique_images)
+    return unique_images, dataset_size, coco
+
+
+def get_image(imageObj, img_folder, input_image_size):
+    # Read and normalize an image
+    train_img = io.imread(img_folder + '/' + imageObj['file_name']) / 255.0
+    # Resize
+    train_img = cv2.resize(train_img, input_image_size)
+    if len(train_img.shape) == 3 and train_img.shape[2] == 3:  # If it is a RGB 3 channel image
+        return train_img
+    else:  # To handle a black and white image, increase dimensions to 3
+        stacked_img = np.stack((train_img,) * 3, axis=-1)
+        return stacked_img
+
+
+def get_normal_mask(imageObj, classes, coco, catIds, input_image_size):
+    annIds = coco.getAnnIds(imageObj['id'], catIds=catIds, iscrowd=None)
+    anns = coco.loadAnns(annIds)
+    cats = coco.loadCats(catIds)
+    train_mask = np.zeros(input_image_size)
+
+    for a in range(len(anns)):
+        className = get_class_name(anns[a]['category_id'], cats)
+        pixel_value = classes.index(className) + 1
+        new_mask = cv2.resize(coco.annToMask(anns[a]) * pixel_value, input_image_size)
+        train_mask = np.maximum(new_mask, train_mask)
+
+    # Add extra dimension for parity with train_img size [X * X * 3]
+    train_mask = train_mask.reshape(input_image_size[0], input_image_size[1], 1)
+    return train_mask
+
+
+def get_binary_mask(imageObj, coco, catIds, input_image_size):
+    annIds = coco.getAnnIds(imageObj['id'], catIds=catIds, iscrowd=None)
+    anns = coco.loadAnns(annIds)
+    train_mask = np.zeros(input_image_size)
+
+    for a in range(len(anns)):
+        new_mask = cv2.resize(coco.annToMask(anns[a]), input_image_size)
+        # Threshold because resizing may cause extraneous values
+        new_mask[new_mask >= 0.5] = 1
+        new_mask[new_mask < 0.5] = 0
+        train_mask = np.maximum(new_mask, train_mask)
+
+    # Add extra dimension for parity with train_img size [X * X * 3]
+    train_mask = train_mask.reshape(input_image_size[0], input_image_size[1], 1)
+    return train_mask
+
+
+def augment_data(gen, augGeneratorArgs, seed=None):
+    # Initialize the image data generator with args provided
+    image_gen = ImageDataGenerator(**augGeneratorArgs)
+    # Remove the brightness argument for the mask. Spatial arguments similar to image.
+    augGeneratorArgs_mask = augGeneratorArgs.copy()
+    _ = augGeneratorArgs_mask.pop('brightness_range', None)
+    # Initialize the mask data generator with modified args
+    mask_gen = ImageDataGenerator(**augGeneratorArgs_mask)
+    np.random.seed(seed if seed is not None else np.random.choice(range(9999)))
+
+    for img, mask in gen:
+        seed = np.random.choice(range(9999))
+        # keep the seeds syncronized otherwise the augmentation of the images
+        # will end up different from the augmentation of the masks
+        g_x = image_gen.flow(255 * img,
+                             batch_size=img.shape[0],
+                             seed=seed,
+                             shuffle=True)
+        g_y = mask_gen.flow(mask,
+                            batch_size=mask.shape[0],
+                            seed=seed,
+                            shuffle=True)
+
+        img_aug = next(g_x) / 255.0
+        mask_aug = next(g_y)
+        yield img_aug, mask_aug
+
+
+def dataloader(images, classes, coco, folder,
+               input_image_size=(224, 224), batch_size=4, mode='train', mask_type='binary'):
+    img_folder = '{}/images/{}'.format(folder, mode)
+    dataset_size = len(images)
+    catIds = coco.getCatIds(catNms=classes)
+
+    c = 0
+    while True:
+        img = np.zeros((batch_size, input_image_size[0], input_image_size[1], 3)).astype('float')
+        mask = np.zeros((batch_size, input_image_size[0], input_image_size[1], 1)).astype('float')
+
+        for i in range(c, c + batch_size):  # initially from 0 to batch_size, when c = 0
+            imageObj = images[i]
+            # Retrieve image
+            train_img = get_image(imageObj, img_folder, input_image_size)
+            # Create mask
+            if mask_type == "binary":
+                train_mask = get_binary_mask(imageObj, coco, catIds, input_image_size)
+            else:  # mask_type == "nomal"
+                train_mask = get_normal_mask(imageObj, classes, coco, catIds, input_image_size)
+            # Add to respective batch sized arrays
+            img[i - c] = train_img
+            mask[i - c] = train_mask
+
+        c += batch_size
+        if c + batch_size >= dataset_size:
+            c = 0
+            random.shuffle(images)
+        yield img, mask
