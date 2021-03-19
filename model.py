99d17d052a7089ab9a080fedb59918035c9085c0
ricky-ma <mr.rickyma@gmail.com>
2021-03-18 18:18:12 -0700

    Fixed model to take 3-channel RGB images

--- model.py
+++ model.py
@@ -9,23 +9,22 @@ class CVAE(tf.keras.Model):
         self.latent_dim = latent_dim
         self.encoder = tf.keras.Sequential(
             [
-                tf.keras.layers.InputLayer(input_shape=(None, None, 3)),
+                tf.keras.layers.InputLayer(input_shape=(224, 224, 3)),
                 tf.keras.layers.Conv2D(
                     filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
                 tf.keras.layers.Conv2D(
                     filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
-                # tf.keras.layers.Flatten(),
-                tf.keras.layers.GlobalMaxPooling2D(),
+                tf.keras.layers.Flatten(),
                 # No activation
                 tf.keras.layers.Dense(latent_dim + latent_dim),
             ]
         )
-
+        print(self.encoder.summary())
         self.decoder = tf.keras.Sequential(
             [
                 tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
-                tf.keras.layers.Dense(units=7 * 7 * 32, activation=tf.nn.relu),
-                tf.keras.layers.Reshape(target_shape=(7, 7, 32)),
+                tf.keras.layers.Dense(units=56 * 56 * 32, activation=tf.nn.relu),
+                tf.keras.layers.Reshape(target_shape=(56, 56, 32)),
                 tf.keras.layers.Conv2DTranspose(
                     filters=64, kernel_size=3, strides=2, padding='same',
                     activation='relu'),
@@ -34,9 +33,10 @@ class CVAE(tf.keras.Model):
                     activation='relu'),
                 # No activation
                 tf.keras.layers.Conv2DTranspose(
-                    filters=1, kernel_size=3, strides=1, padding='same'),
+                    filters=3, kernel_size=3, strides=1, padding='same'),
             ]
         )
+        print(self.decoder.summary())
 
     @tf.function
     def sample(self, eps=None):
