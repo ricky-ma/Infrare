0f4857d2bcbe1c236480915ebdac8baef02380d9
ricky-ma <mr.rickyma@gmail.com>
2021-03-21 18:08:50 -0700

    Added checkpoint to save weights during training

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 from models.cvae import CVAE
-from preprocessing import filter_dataset, dataloader, augment_data
+from preprocessing import dataloader
 
 
 def log_normal_pdf(sample, mean, logvar, raxis=1):
@@ -40,69 +40,51 @@ def train_step(model, x, optimizer):
     return loss
 
 
-def generate_and_save_images(model, epoch, test_sample):
-    mean, logvar = model.encode(test_sample)
+def generate_and_save_images(model, epoch, batch, train):
+    batch_imgs, batch_labels = batch
+    mean, logvar = model.encode(batch_imgs)
     z = model.reparameterize(mean, logvar)
     predictions = model.sample(z)
-    num_col = test_sample.shape[0]
+    num_col = batch_imgs.shape[0]
     fig = plt.figure(figsize=(num_col, 2))
 
     for i in range(num_col):
-        plt.subplot(2, num_col, i + 1)
-        plt.imshow(test_sample[i, :, :, :])
+        fig = plt.subplot(2, num_col, i + 1)
+        plt.imshow(batch_imgs[i, :, :, :])
         plt.axis('off')
 
-    for i in range(num_col):
         plt.subplot(2, num_col, i + 1 + num_col)
         plt.imshow(predictions[i, :, :, :])
         plt.axis('off')
 
-    # tight_layout minimizes the overlap between 2 sub-plots
-    plt.savefig('vae/results/image_at_epoch_{:04d}.png'.format(epoch))
+        fig.set_title(int(batch_labels[i]))
+
+    if train:
+        plt.savefig('vae/train_out/image_at_epoch_{:04d}.png'.format(epoch))
+    else:
+        plt.savefig('vae/test_out/image_at_epoch_{:04d}.png'.format(epoch))
     plt.show()
 
 
 if __name__ == "__main__":
     data_dir = 'coco2017'
-    classes = ['airplane', 'train', 'car', 'truck', 'bus']
+    classes = ['apple', 'orange']
     mode = 'val2017'
     model_dir = 'vae/'
     log_dir = 'logs'
     input_image_size = (512, 512, 3)
-    mask_img = True
     batch_size = 10
     epochs = 2
     latent_dim = 32
     optimizer = tf.keras.optimizers.Adam(1e-3)
 
     # load and augment training data
-    images, dataset_size, coco = filter_dataset(data_dir, classes, mode)
-    train_gen = dataloader(images, classes, coco, data_dir, input_image_size, batch_size, mode, mask_img)
-    augGeneratorArgs = dict(featurewise_center=False,
-                            samplewise_center=False,
-                            rotation_range=5,
-                            width_shift_range=0.01,
-                            height_shift_range=0.01,
-                            brightness_range=(0.8, 1.2),
-                            shear_range=0.01,
-                            zoom_range=[1, 1.25],
-                            horizontal_flip=True,
-                            vertical_flip=False,
-                            fill_mode='reflect',
-                            data_format='channels_last')
-    ds_train = augment_data(train_gen, augGeneratorArgs)
+    ds_train = dataloader(classes, data_dir, input_image_size, batch_size, mode)
 
     # initialize and compile model
     model = CVAE(latent_dim, input_image_size)
     callback_list = [
-        tf.keras.callbacks.TensorBoard(log_dir=log_dir),
-        tf.keras.callbacks.ModelCheckpoint(
-            filepath=os.path.join(model_dir, "weights", "step{step}"),
-            monitor="loss",
-            save_best_only=False,
-            save_weights_only=True,
-            verbose=1,
-        )
+        tf.keras.callbacks.TensorBoard(log_dir=log_dir)
     ]
     TC = tf.keras.callbacks.CallbackList(callbacks=callback_list, model=model)
     current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -114,7 +96,7 @@ if __name__ == "__main__":
     # Pick a sample of the test set for generating output images
     # assert batch_size >= num_examples_to_generate
     test_batch = next(ds_train)
-    generate_and_save_images(model, 0, test_batch)
+    generate_and_save_images(model, 0, test_batch, True)
 
     # Iterate over epochs.
     for epoch in range(1, epochs + 1):
@@ -123,17 +105,21 @@ if __name__ == "__main__":
         # Iterate over the batches of the dataset.
         start_time = time.time()
         for step, train_x in enumerate(ds_train):
-            loss = train_step(model, train_x, optimizer)
+            imgs, labels = train_x
+            loss = train_step(model, imgs, optimizer)
             with train_summary_writer.as_default():
                 tf.summary.scalar('loss', loss, step=step)
             if step % 100 == 0:
                 print("step %d: mean loss = %.4f" % (step, loss))
-                generate_and_save_images(model, step, test_batch)
+                generate_and_save_images(model, step, test_batch, True)
+                checkpoint_dir = os.path.dirname('vae/checkpoints/cp-{step:04d}')
+                model.save_weights(checkpoint_dir)
         end_time = time.time()
 
         # Calculate reconstruction error.
         loss = tf.keras.metrics.Mean()
         for step, test_x in enumerate(ds_train):
+            imgs, labels = test_x
             loss(compute_loss(model, test_x))
             with test_summary_writer.as_default():
                 tf.summary.scalar('loss', loss, step=step)
