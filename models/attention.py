284dc7233d3e05d602e553ed94831206657b05cd
ricky-ma <mr.rickyma@gmail.com>
2021-03-24 19:36:28 -0700

    CVAE with attention mechanism

---
+++ models/attention.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+
+
+class AttentionCVAE(tf.keras.Model):
+    """Convolutional variational autoencoder."""
+
+    def __init__(self, latent_dim, input_shape):
+        super(AttentionCVAE, self).__init__()
+        # self.batch_sz = input_shape[0]
+        # self.enc_units = input_shape[1:]
+        self.latent_dim = latent_dim
+        # self.hidden = tf.zeros((self.batch_sz, self.enc_units))
+        self.attention = tf.keras.layers.Attention()
+        self.encoder = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=input_shape),
+                tf.keras.layers.Conv2D(
+                    filters=32, kernel_size=3, strides=2, activation='relu'),
+                tf.keras.layers.Conv2D(
+                    filters=64, kernel_size=3, strides=2, activation='relu'),
+                tf.keras.layers.Conv2D(
+                    filters=128, kernel_size=3, strides=2, activation='relu'),
+                tf.keras.layers.Conv2D(
+                    filters=256, kernel_size=4, strides=2, activation='relu'),
+                tf.keras.layers.Flatten(),
+                # tf.keras.layers.GlobalMaxPool2D(),
+                # No activation
+                tf.keras.layers.Dense(latent_dim + latent_dim),
+            ]
+        )
+        print(self.encoder.summary())
+        self.decoder = tf.keras.Sequential(
+            [
+                tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+                tf.keras.layers.Dense(units=16 * 16 * 32, activation=tf.nn.relu),
+                tf.keras.layers.Reshape(target_shape=(16, 16, 32)),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=256, kernel_size=4, strides=2, padding='same',
+                    activation='relu'),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=128, kernel_size=3, strides=2, padding='same',
+                    activation='relu'),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=64, kernel_size=3, strides=2, padding='same',
+                    activation='relu'),
+                tf.keras.layers.Conv2DTranspose(
+                    filters=32, kernel_size=3, strides=2, padding='same',
+                    activation='relu'),
+                # No activation
+                tf.keras.layers.Conv2DTranspose(
+                    filters=3, kernel_size=3, strides=1, padding='same'),
+            ]
+        )
+        print(self.decoder.summary())
+
+    @tf.function
+    def sample(self, eps=None):
+        if eps is None:
+            eps = tf.random.normal(shape=(100, self.latent_dim))
+        return self.decode(eps, apply_sigmoid=True)
+
+    def encode(self, x):
+        x = tf.keras.layers.Attention()(inputs=[x, x])
+        mean, logvar = tf.split(self.encoder(x), num_or_size_splits=2, axis=1)
+        return mean, logvar
+
+    def reparameterize(self, mean, logvar):
+        eps = tf.random.normal(shape=mean.shape)
+        return eps * tf.exp(logvar * .5) + mean
+
+    def decode(self, z, apply_sigmoid=False):
+        logits = self.decoder(z)
+        if apply_sigmoid:
+            probs = tf.sigmoid(logits)
+            return probs
+        return logits
